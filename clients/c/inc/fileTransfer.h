ecd1de2f1d26709f535fb03f602fb8ab95bd652b
imorrisonca <38136349+imorrisonca@users.noreply.github.com>
2021-04-06 08:36:42 -0700

    Update fileTransfer.h

--- clients/c/inc/fileTransfer.h
+++ clients/c/inc/fileTransfer.h
@@ -8,6 +8,7 @@ enum fileTransferEventE
     FILES_TRANSFER_EVENT_INFO,                ///< Informational (ignored by ORP service)
     FILES_TRANSFER_EVENT_READY,               ///< Client ready for file transfer
     FILES_TRANSFER_EVENT_PENDING,             ///< Transfer pending
+    FILES_TRANSFER_EVENT_ACCEPT,              ///< File transfer accept
     FILES_TRANSFER_EVENT_SUSPEND,             ///< File transfer suspend
     FILES_TRANSFER_EVENT_RESUME,              ///< File transfer resume
     FILES_TRANSFER_EVENT_COMPLETE,            ///< File transfer complete
